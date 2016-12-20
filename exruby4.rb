f449d344160bea16b27b9f6e79b4b681a2142d99
darchi07 <kokuryousei@gmail.com>
2016-12-21 03:34:59 +0900

    解答・説明分編集

--- exruby4.rb
+++ exruby4.rb
@@ -1,8 +1,11 @@
 #!/usr/bin/ruby
+# coding: utf-8
 str = nil
 arry1=Array.new
 while str = STDIN.gets
-#  str.slice!(str.length-1,str.length)
+  if str.include?("\n")
+    str.slice!(str.length-1,str.length)
+  end
   str =str.split(" ")
   break if str != nil
   
@@ -19,3 +22,5 @@ a,b = 0, 0
 a = Rational(str[0].to_i,str[1].to_i)
 b = Rational(str[2].to_i,str[3].to_i)
 puts a+b
+
+#標準入力に分数が2つ入力された時に正しい結果を約分済みの分数で示せ

f5d5f1e8d1d23eda5dee2f54866ece805899e1ea
darchi07 <kokuryousei@gmail.com>
2016-12-21 03:29:41 +0900

    説明文・解答追加

--- exruby3.rb
+++ exruby3.rb
@@ -1,5 +1,9 @@
+# coding: utf-8
+#filenameの文字列の長さの結果が8で出力されるのはどれ？
 filename = "text.txt"
 
 puts "length :#{filename.length}"
 puts "size: #{filename.size}"
 puts "count : #{filename.count}"
+
+#答え：lengthとsize，countは配列の要素数を返す関数
